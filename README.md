722bb40da71a65a94db2ee0d39defd6dc98d1883
Chatchai Jantaraprim <chatchai.j@gmail.com>
2023-01-08 11:41:23 +0700

    add script and change README.md

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # upwork-linux-hidraw-configuration
+
+This version of script should work with Debian 11 / kernel 5.10.x
+The script should be run like this
+
+```bash
+git clone $THIS\_REPO
+cd $THIS\_REPO
+bash build-new-kernel.sh
+```
+
+or if using different kernel version and config
+
+```bash
+SOURCEDIR=linux-$VERSION CONFIG=/boot/config-$VERSION bash-new-kernel.sh
+```
+
---
+++ build-new-kernel.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+
+SOURCEDIR="${SOURCEDIR:-linux-5.10.158}"
+CONFIG="${CONFIG:-/boot/config-5.10.0-18-amd64}"
+
+## System preparation
+apt-get update
+
+# get packages necessary for building debian package from sources
+apt-get -y install build-essential fakeroot
+
+# get dependency packages that necessary for building linux kernel package
+apt-get -y build-dep linux
+
+# get linux kernel sources from debian repository
+apt-get source linux
+
+if [ ! -d "$SOURCEDIR" ]; then
+	cat <<EOT
+ERROR:
+Expect $SOURCEDIR directory for kernel source, but can not find it
+Please check the directory below, and modify SOURCEDIR var at
+the top of this script.
+EOT
+	ls -l
+	exit
+fi
+
+cd $SOURCEDIR
+
+# prepare for building, we will build kernel for amd64 only
+fakeroot make -f debian/rules.gen setup_amd64_none_amd64
+
+### HERE we change HIDRAW_MAX_DEVICES from 64 to 256 ###
+sed -i -e 's/HIDRAW_MAX_DEVICES 64/HIDRAW_MAX_DEVICES 256/' include/uapi/linux/hidraw.h
+
+### Using existing kernel config as template ###
+cat $CONFIG | \
+sed	-e '/CONFIG_INTEGRITY_PLATFORM_KEYRING/d' \
+	-e '/CONFIG_LOAD_UEFI_KEYS/d' \
+	-e '/CONFIG_SECONDARY_TRUSTED_KEYRING/d' \
+	-e '/CONFIG_SYSTEM_BLACKLIST_KEYRING/d' \
+	-e 's/CONFIG_SYSTEM_TRUSTED_KEYS=.\+/CONFIG_SYSTEM_TRUSTED_KEYS=""/' \
+> .config
+
+### This is ugly, but should work for now! ###
+printf "\n\n\n\n\n" | make oldconfig
+
+make clean
+
+### now do the real build ###
+make -j `getconf _NPROCESSORS_ONLN` bindeb-pkg LOCALVERSION=-custom
